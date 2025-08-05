d2e8722ad087af8fca076bb3ba37c01e51b087a2
Joshua Adeyemi <joshuaadedolapo@gmail.com>
2025-08-05 22:06:25 +0100

    fix: redirect route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-        $middleware->redirectUsersTo(route('user.dashboard'));
         $middleware->alias([
             'admin' => \App\Http\Middleware\Admin::class,
             'admin.guest' => \App\Http\Middleware\AdminGuest::class,
