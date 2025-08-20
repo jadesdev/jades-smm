01d11dd5d042034cfc69c6c7e3c677ce0bfc8405
Joshua Adeyemi <joshuaadedolapo@gmail.com>
2025-08-20 23:24:44 +0100

    refactor: use foreignUlid for user_id in tickets
    - Use ulid for user_id
    - Consistent primary key type

--- database/migrations/2025_07_09_021513_create_support_tickets_table.php
+++ database/migrations/2025_07_09_021513_create_support_tickets_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('support_tickets', function (Blueprint $table) {
             $table->ulid('id')->primary();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignUlid('user_id')->constrained()->onDelete('cascade');
             $table->string('code')->unique();
             $table->string('last_reply')->default('user');
             $table->string('subject');
